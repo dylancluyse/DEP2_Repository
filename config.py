d7a4885db156bcaff4a1dcbb88acf080d32eb94c
liwago <liam.goethals@wegroup.be>
2022-11-20 19:44:15 +0100

    Add dotenv implementation to hide secrets

---
+++ config.py
@@ -0,0 +1,16 @@
+import os 
+try:  # nosec
+    from dotenv import load_dotenv
+
+    load_dotenv(verbose=True)
+except Exception:  # nosec
+    pass
+
+class Config():
+    DATABASE: str = os.getenv("DB_NAME")
+    USER: str = os.getenv("DB_USER")
+    HOST: str = os.getenv("DB_HOST")
+    PORT: str = os.getenv("DB_PORT")
+    PASSWORD: str = os.getenv("DB_PASSWORD")
+
+config = Config()
