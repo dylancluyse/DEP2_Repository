a6b5a32de45412b934aed530bd5afbf8075bbe05
liwago <liam.goethals@wegroup.be>
2022-11-24 16:10:47 +0100

    Add self reference to connection class

--- Controllers/Repositories/ConnectionController.py
+++ Controllers/Repositories/ConnectionController.py
@@ -4,5 +4,5 @@ import pyodbc, psycopg2
 from config import config
 
 class Connection():
-    def get_conn():
+    def get_conn(self):
         return psycopg2.connect(database=config.DATABASE, user=config.USER, password=config.PASSWORD, host=config.HOST, port=config.PORT)
