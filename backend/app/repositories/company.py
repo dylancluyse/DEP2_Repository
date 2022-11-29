79a2829fcbcf8dea4048d979136ee038ac3a55d2
liwago <liam.goethals@wegroup.be>
2022-11-29 22:09:45 +0100

    Add Company Repository

---
+++ backend/app/repositories/company.py
@@ -0,0 +1,19 @@
+from repositories import BaseRepository
+
+
+class CompanyRespository(BaseRepository):
+
+    def __init__(self, db):
+        self.db = db
+
+    def fetch_all_company_names(self):
+        q = '''SELECT naam from "KMO"'''
+        v = []
+        result = self.fetch_all(q, v)
+        return result
+
+    def fetch_company(self, naam):
+        q = '''SELECT * from "KMO" where "naam" = (%s)'''
+        v = [naam]
+        result = self.fetch_all(q, v)
+        return result
