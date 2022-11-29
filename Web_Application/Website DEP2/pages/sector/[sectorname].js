e3249f67454766f0d63d7cf6e9e071bf4c13b9d0
liwago <liam.goethals@wegroup.be>
2022-11-29 23:49:27 +0100

    Implement sector routes

--- Web_Application/Website DEP2/pages/sector/[sectorname].js
+++ Web_Application/Website DEP2/pages/sector/[sectorname].js
@@ -1,5 +1,6 @@
+import { Link } from '@mui/material'
 import { useRouter } from 'next/router'
-import BasicList from '../../components/company-list-card.js'
+import CompanyList from '../../components/company-list-card.js'
 
 const Post = () => {
   const router = useRouter()
@@ -7,11 +8,11 @@ const Post = () => {
 
   return (
     <div>
-      <a href="/">Home </a>
+      <Link href="/">Home </Link>
       <h1> {sectorname} </h1>
       <br/>
       <br/>
-      <BasicList/ >
+      <CompanyList sector={sectorname} / >
     </div>
   )
 }
