f9640f72d8e1e141cdc755bacc52ebf1d8ad8a12
liwago <liam.goethals@wegroup.be>
2022-11-29 22:09:10 +0100

    Add database connection to BE

---
+++ backend/app/database/db.py
@@ -0,0 +1,18 @@
+from typing import Any
+from config import config
+import psycopg2
+
+
+
+class DB():
+    conn: Any
+
+    def setup(self):
+        self.conn = psycopg2.connect(
+            database=config.DATABASE,
+            user=config.USER,
+            password=config.PASSWORD,
+            host=config.HOST, 
+            port=config.PORT
+        )
+
