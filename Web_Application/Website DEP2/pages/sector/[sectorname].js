b3a937a71e8c6132bfc5d64915cbfcbd09dc01de
Szymon <szymon.vertenten@student.hogent.be>
2022-12-04 15:50:25 +0100

    update

--- Web_Application/Website DEP2/pages/sector/[sectorname].js
+++ Web_Application/Website DEP2/pages/sector/[sectorname].js
@@ -9,17 +9,23 @@ const Post = () => {
 
   return (
     <div class='overflow-hidden	'>
-      <Link href='/' class='sticky top-0'>
-        Home{' '}
-      </Link>
+      <div>
+        <a
+          href='/'
+          class=' fixed mb-5 m-5 text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+        >
+          back
+        </a>
+        <h1 class='sticky top-0 p-2 mx-10 mt-5 text-center text-2xl font-bold	'>
+          {' '}
+          {sectorname}{' '}
+        </h1>
+      </div>
 
-      <h1 class='static top-0 p-2 mx-10  bg-red-500 text-center text-2xl	font-bold	'>
-        {' '}
-        {sectorname}{' '}
-      </h1>
       <br />
       <br />
-      <div class='text-left pl-2 flex flex-row max-h-screen 	'>
+
+      <div class='absolute text-left pl-2 flex flex-row max-h-screen w-full	'>
         <CompanyList sector={sectorname} />
         <div class=' bg-gradient-to-r from-oxford-blue to-lighter-oxford w-full text-white	'>
           {/* plaats voor gegevens bedrijf + grafieken */}
