38a1a47ee9d2b810e6adfa30467a530386ba9140
liwago <liam.goethals@wegroup.be>
2022-12-15 12:36:03 +0100

    Reimplement fetch company query with the view

--- backend/app/repositories/sector.py
+++ backend/app/repositories/sector.py
@@ -4,7 +4,7 @@ from app.repositories import BaseRepository
 class SectorRepository(BaseRepository):
 
     def fetch_sectors(self):
-        result = self.fetch_all('''SELECT sectornaam from "Sector"''', [])
+        result, description = self.fetch_all('''SELECT sectornaam from "Sector"''', [])
         return result
 
     # def fetch_sector(self, sector_naam):
@@ -12,7 +12,7 @@ class SectorRepository(BaseRepository):
     #     return result
 
     def fetch_company_by_sector(self, sector_naam):
-        result = self.fetch_all('''select naam, ondernemingsnummer from "KMO" join "Sector" ON "Sector".sectornummer = "KMO".sectorid WHERE "Sector".sectornaam = (%s)''',[sector_naam])
+        result, description = self.fetch_all('''select naam, ondernemingsnummer from "KMO" join "Sector" ON "Sector".sectornummer = "KMO".sectorid WHERE "Sector".sectornaam = (%s)''',[sector_naam])
         # return [company[0] for company in result]
         return [{"naam": company[0], "ondernemingsnummer": company[1]} for company in result]
         # return resrlt
