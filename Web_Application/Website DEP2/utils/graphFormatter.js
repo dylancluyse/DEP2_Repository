1b3aa3c4d2bfd8fa32e257137569ccf547827652
liwago <liam.goethals@student.hogent.be>
2022-12-18 12:29:28 +0100

    Add util functions for sector data fetching and graph formatting

---
+++ Web_Application/Website DEP2/utils/graphFormatter.js
@@ -0,0 +1,16 @@
+
+function toPercentage(number) {
+  return (number * 100).toFixed(2) 
+}
+
+export const formatGraphDataToPercentages = (data, keys) => {
+
+  return data.map(item => {
+    const formattedItem = { ...item }
+    keys.forEach(key => {
+      formattedItem[key] = toPercentage(item[key])
+    });
+    return formattedItem;
+  });
+};
+
