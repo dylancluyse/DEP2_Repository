26cee8fcec49dd2e860986ed78690c3ebcc68df6
liwago <liam.goethals@student.hogent.be>
2022-12-20 21:28:47 +0100

    Add ML models to BE for admin portal

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,4 @@
+from .loader import load_model_ann_ult_score, load_model_linear_regression, load_model_rfr_general_score
+
+
+__all__ = ["load_model_ann_ult_score", "load_model_linear_regression", "load_model_rfr_general_score"]
