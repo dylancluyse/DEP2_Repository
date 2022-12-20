16233d3ebb7f83c73844f244c876d24832a5fb37
Szymon <szymon.vertenten@student.hogent.be>
2022-12-20 22:39:14 +0100

    makeover keywords

--- Web_Application/Website DEP2/pages/admin/index.js
+++ Web_Application/Website DEP2/pages/admin/index.js
@@ -2,19 +2,32 @@ import { Link, Button } from '@mui/material';
 import { useRouter } from 'next/router';
 
 const AdminPortal = () => {
-
-
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2  max-h-screen bg-Grijs'>
-      <main className='flex flex-col items-center justify-center w-full flex-1 sm:px-20 my-10 '>
-        <a href='/' class='mt-2 mb-5 m-5 text-white bg-lichtblauw font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  z-10' >
-          Home
-        </a>
-        <h1 className='text-4xl sm:text-6xl font-bold'>Admin - SUOR SGE Portal Site</h1>
+    <div className=' min-h-screen py-2  max-h-screen bg-Grijs'>
+      <a
+        href='/'
+        class=' absolute mt-2 mb-5 m-5 text-white bg-lichtblauw font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  z-10'
+      >
+        Home
+      </a>
+      <main className='mt-28 flex flex-col items-center justify-center w-full flex-1 sm:px-20 my-10 '>
+        <h1 className='text-4xl sm:text-6xl font-bold'>
+          Admin - SUOR SGE Portal Site
+        </h1>
 
-        <div>
-          <Button href="/admin/keywords" className='mt-2 mb-5 m-5 text-white bg-lichtblauw hover:bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  z-10'>Add keywords</Button>
-          <Button href="admin/predict" className='mt-2 mb-5 m-5 text-white bg-lichtblauw hover:bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  z-10'>Predict company</Button>
+        <div className='space-x-10 mt-5'>
+          <Button
+            href='/admin/keywords'
+            className=' m-5 text-white bg-lichtblauw hover:bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  z-10'
+          >
+            Add keywords
+          </Button>
+          <Button
+            href='admin/predict'
+            className=' m-5 text-white bg-lichtblauw hover:bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  z-10'
+          >
+            Predict company
+          </Button>
         </div>
       </main>
     </div>
--- Web_Application/Website DEP2/pages/admin/keywords.js
+++ Web_Application/Website DEP2/pages/admin/keywords.js
@@ -2,55 +2,55 @@ import { Box } from '@mui/material';
 import useSWR from 'swr';
 import fetcher from '../../lib/fetcher';
 
-
 const parseSubDomain = (subdomain, keywordList) => {
-
   const render = [];
   for (const subdom of Object.entries(keywordList.value)) {
-    render.push(<li className='font-normal'>{subdom[1]}</li>)
+    render.push(<li className='font-normal'>{subdom[1]}</li>);
   }
 
   return (
-    <li className="pb-2 font-bold">{subdomain}
-      <Box component="ul" aria-labelledby="category-a" sx={{ pl: 2 }} >
+    <li className='pb-2 font-bold'>
+      {subdomain}
+      <Box component='ul' aria-labelledby='category-a' sx={{ pl: 2 }}>
         {render}
       </Box>
     </li>
-  )
-}
+  );
+};
 
 const createKeywordDomainList = (domain, keywords) => {
   const output = [];
   for (const [subdomain, subdomainKeywords] of Object.entries(keywords)) {
-    output.push(parseSubDomain(subdomain, subdomainKeywords))
+    output.push(parseSubDomain(subdomain, subdomainKeywords));
   }
 
   return (
-    <Box component="ul" aria-labelledby="category-a" sx={{ pl: 2 }} className='border-2 ml-5'>
-      <li>{domain}
-        <Box component="ul" aria-labelledby="category-a" sx={{ pl: 2 }}>
+    <Box
+      component='ul'
+      aria-labelledby='category-a'
+      sx={{ pl: 2 }}
+      className='border-2 p-2 '
+    >
+      <li>
+        {domain}
+        <Box component='ul' aria-labelledby='category-a' sx={{ pl: 2 }}>
           {output}
         </Box>
       </li>
     </Box>
-  )
-}
+  );
+};
 
 const createKeywordList = (fullList) => {
-  console.log(fullList)
-  const output = []
+  console.log(fullList);
+  const output = [];
   for (const [domain, keywords] of Object.entries(fullList)) {
-    output.push(createKeywordDomainList(domain, keywords))
+    output.push(createKeywordDomainList(domain, keywords));
   }
-  return (
-    <div className="flex">
-      {output}
-    </div>
-  )
-}
+  return <div className='flex mx-auto space-x-5 mb-10'>{output}</div>;
+};
 
 export default function keywords() {
-
   const { data: keywords, error } = useSWR(
     `http://localhost:8000/admin/keywords`,
     fetcher
@@ -58,18 +58,15 @@ export default function keywords() {
   if (error) return <div>failed to load</div>;
   if (!keywords) return <div>loading...</div>;
 
-  const foo = createKeywordList(keywords)
+  const foo = createKeywordList(keywords);
 
   return (
     <div>
       <h1> Keywords </h1>
-      <div className="flex">
-        <p> foo </p>    
+      <div className='flex '>
+        <p> foo </p>
       </div>
-      <div className="flex">
-        {foo}    
-      </div>
+      <div className='flex mx-auto '>{foo}</div>
     </div>
-  )
+  );
 }
-
