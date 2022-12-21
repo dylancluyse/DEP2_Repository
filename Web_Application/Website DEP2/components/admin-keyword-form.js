3e60dbcbcee3946a6af86bda67b3dc7f90c8bd11
liwago <liam.goethals@student.hogent.be>
2022-12-21 01:40:19 +0100

    Implement final prediction and keyword form

--- Web_Application/Website DEP2/components/admin-keyword-form.js
+++ Web_Application/Website DEP2/components/admin-keyword-form.js
@@ -1,5 +1,7 @@
+import { useRouter } from 'next/router'
 import {
   Box,
+  Button,
   FormControl,
   FormHelperText,
   Input,
@@ -14,13 +16,6 @@ import fetcher from '../lib/fetcher';
 export default function AdminKeywordForm(props) {
   const { categories } = props;
 
-  // const { data: keywords, error } = useSWR(
-  //   `http://localhost:8000/admin/keywords`,
-  //   fetcher
-  // );
-  // if (error) return <div>failed to load</div>;
-  // if (!keywords) return <div>loading...</div>;
-  //
   const menuItems = [];
 
   for (const [keyword, id] of Object.entries(categories)) {
@@ -28,40 +23,91 @@ export default function AdminKeywordForm(props) {
   }
 
   const [category, setCategory] = useState('');
+  const [selectedKeyword, setSelectedKeyword] = useState('');
 
   const handleChange = (event) => {
     setCategory(event.target.value);
     console.log(event.target.value);
   };
 
+  const handleInput = (event) => {
+    setSelectedKeyword(event.target.value);
+    console.log(selectedKeyword);
+  };
+
+  const [shouldAdd, setShouldAdd] = useState(false);
+  const [shouldRemove, setShouldRemove] = useState(false);
+
+  const {} = useSWR(
+    !shouldAdd ? null : `http://localhost:8000/admin/keyword/add?category_id=${category}&keyword=${selectedKeyword}`,
+    fetcher
+  );
+
+  const {} = useSWR(
+    !shouldRemove ? null : `http://localhost:8000/admin/keyword/remove?category_id=${category}&keyword=${selectedKeyword}`,
+    fetcher
+  );
+
+  const addKeyword = (event) => {
+    setShouldAdd(true)
+  };
+//  
+
+  const router = useRouter()
+
+  const removeKeyword = (event) => {
+    setShouldRemove(true)
+
+    router.reload(window.location.pathname)
+
+  };
+
   return (
     <div className='snap-center p-5'>
-      <Box component='form' aria-labelledby='category-a' sx={{ pl: 2 }}>
-        <FormControl className='w-44 '>
-          <InputLabel htmlFor='my-input' className=''>
-            Kies een categorie:
-          </InputLabel>
-          <Select
-            labelId='simple-select-label'
-            id='simple-select'
-            value={category}
-            label='Age'
-            onChange={handleChange}
-          >
-            {menuItems}
-          </Select>
-        </FormControl>
-        <div className='mt-5'></div>
+      <Box component='form' onSubmit={addKeyword} className='' aria-labelledby='category-a' sx={{ pl: 2 }}>
+        <div className='flex w-full' >
+          <div className='pr-10 w-1/4'>
+        
+          <FormControl  className='w-full flex mx-10'>
+            <InputLabel htmlFor='my-input' className=''>
+              Kies een categorie:
+            </InputLabel>
+            <Select
+              labelId='simple-select-label'
+              id='simple-select'
+              value={category}
+              label='Age'
+              className=''
+              onChange={handleChange}
+            >
+              {menuItems}
+            </Select>
+          </FormControl>
+
+          </div>
+
+          <div className='pr-10'>
         <FormControl className=' m-5'>
-          <InputLabel htmlFor='my-input-1' className='py-19'>
+          <InputLabel htmlFor='my-input-1' className=''>
             Woord:
           </InputLabel>
           <Input
             id='my-input-1'
-            className='py-19'
+            className='w-full'
             aria-describedby='my-helper-text'
+            onChange={handleInput}
           />
         </FormControl>
+          </div>
+
+          <Button type="submit" className=' m-5 text-white bg-lichtblauw hover:bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  z-10' >
+            Add
+          </Button>
+
+          <Button onClick={removeKeyword} className=' m-5 text-white bg-lichtblauw hover:bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2  z-10' >
+            Remove
+          </Button>
+        </div>
       </Box>
     </div>
   );
